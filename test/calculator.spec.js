8788c7c779e664b587b993768507d1dd0422a73a
kstewart146 <68401145+kstewart146@users.noreply.github.com>
2020-12-16 16:59:31 -0800

    first commit

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -1,8 +1,10 @@
 /* global define, it, describe */
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import App from './../src/js/app';
+import Adapter from 'enzyme-adapter-react-15';
+configure({ adapter: new Adapter() });
 
 describe('App Component', function () {
   this.timeout(6500);
